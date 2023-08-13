a96b9b4d061ce3f99405a8b333ea9305babef1ff
Binit Gupta <binitguptapro@gmail.com>
2023-08-13 09:00:15 +0530

    firebase-setup -successful

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:wbfactory/constants/colors.dart';
 import 'package:wbfactory/views/onboarding_screens/splash.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
