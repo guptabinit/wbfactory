987f95182cc3615800f3ae58a6e250053224c6cf
binitgupta <binitguptapro@gmail.com>
2024-02-03 00:11:29 +0530

    v1.0.4 -done

--- lib/views/home_screens/home_page.dart
+++ lib/views/home_screens/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -13,6 +15,7 @@ import '../other_screens/product_page.dart';
 
 class HomePage extends StatefulWidget {
   final bool userAvailable;
+
   const HomePage({super.key, required this.userAvailable});
 
   @override
@@ -21,11 +24,52 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   List imageList = [
-    {"id": 1, "image_path": 'https://firebasestorage.googleapis.com/v0/b/whitestone-bagel-factory.appspot.com/o/banners%2F32.png?alt=media&token=f3eb973b-b25c-4ba9-92ec-c1a702b2b4ca'},
-    {"id": 2, "image_path": 'https://firebasestorage.googleapis.com/v0/b/whitestone-bagel-factory.appspot.com/o/banners%2F17.png?alt=media&token=32311171-efb9-4193-9018-0a2a3d7d5d4d'},
-    {"id": 3, "image_path": 'https://firebasestorage.googleapis.com/v0/b/whitestone-bagel-factory.appspot.com/o/banners%2F2.png?alt=media&token=c0ec93c8-3302-4d59-b1c1-08f8e1e6f07e'},
-    {"id": 4, "image_path": 'https://firebasestorage.googleapis.com/v0/b/whitestone-bagel-factory.appspot.com/o/banners%2F23.png?alt=media&token=38097785-dc88-4b4c-bd78-bbe273932e25'},
+    {
+      // "id": 1,
+      "image_path":
+          'https://firebasestorage.googleapis.com/v0/b/whitestone-bagel-factory.appspot.com/o/banners%2F32.png?alt=media&token=f3eb973b-b25c-4ba9-92ec-c1a702b2b4ca'
+    },
+    {
+      // "id": 2,
+      "image_path":
+          'https://firebasestorage.googleapis.com/v0/b/whitestone-bagel-factory.appspot.com/o/banners%2F17.png?alt=media&token=32311171-efb9-4193-9018-0a2a3d7d5d4d'
+    },
+    {
+      // "id": 3,
+      "image_path":
+          'https://firebasestorage.googleapis.com/v0/b/whitestone-bagel-factory.appspot.com/o/banners%2F2.png?alt=media&token=c0ec93c8-3302-4d59-b1c1-08f8e1e6f07e'
+    },
+    {
+      // "id": 4,
+      "image_path":
+          'https://firebasestorage.googleapis.com/v0/b/whitestone-bagel-factory.appspot.com/o/banners%2F23.png?alt=media&token=38097785-dc88-4b4c-bd78-bbe273932e25'
+    },
   ];
+
+  @override
+  void initState() {
+
+    getData();
+
+    super.initState();
+  }
+
+  getData() async {
+
+    try {
+      var snap = await FirebaseFirestore.instance
+          .collection('commons')
+          .doc('banners')
+          .get();
+
+      setState(() {
+        imageList = snap.data()!['banner_links'];
+      });
+    } catch (e) {
+      print("ERROR: Error in fetching");
+    }
+
+  }
 
   bool showDeals = false;
 
@@ -36,18 +80,22 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
-        physics: const BouncingScrollPhysics(decelerationRate: ScrollDecelerationRate.fast),
+        physics: const BouncingScrollPhysics(
+            decelerationRate: ScrollDecelerationRate.fast),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             8.heightBox,
             GestureDetector(
               onTap: () {
-                Get.to(() => SearchScreen(userAvailable: widget.userAvailable,));
+                Get.to(() => SearchScreen(
+                      userAvailable: widget.userAvailable,
+                    ));
               },
               child: Container(
                 margin: const EdgeInsets.symmetric(horizontal: 12),
-                padding: const EdgeInsets.symmetric(vertical: 14, horizontal: 12),
+                padding:
+                    const EdgeInsets.symmetric(vertical: 14, horizontal: 12),
                 decoration: BoxDecoration(
                   color: veryLightGreyColor,
                   borderRadius: BorderRadius.circular(8),
@@ -88,8 +136,13 @@ class _HomePageState extends State<HomePage> {
                           (item) => CachedNetworkImage(
                             imageUrl: item["image_path"],
                             fit: BoxFit.cover,
-                            progressIndicatorBuilder: (context, url, downloadProgress) => Center(child: CircularProgressIndicator(value: downloadProgress.progress)),
-                            errorWidget: (context, url, error) => const Center(child: Icon(Icons.error)),
+                            progressIndicatorBuilder:
+                                (context, url, downloadProgress) => Center(
+                              child: CircularProgressIndicator(
+                                  value: downloadProgress.progress),
+                            ),
+                            errorWidget: (context, url, error) =>
+                                const Center(child: Icon(Icons.error)),
                             width: double.infinity,
                           ),
                         )
@@ -122,7 +175,9 @@ class _HomePageState extends State<HomePage> {
                       margin: const EdgeInsets.symmetric(horizontal: 4),
                       decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(10),
-                        color: currentIndex == entry.key ? secondaryColor : lightGreyColor,
+                        color: currentIndex == entry.key
+                            ? secondaryColor
+                            : lightGreyColor,
                       ),
                     ));
               }).toList(),
@@ -153,7 +208,9 @@ class _HomePageState extends State<HomePage> {
                   padding: const EdgeInsets.symmetric(vertical: 12),
                   decoration: BoxDecoration(
                     color: whiteColor,
-                    borderRadius: const BorderRadius.only(topRight: Radius.circular(12), bottomRight: Radius.circular(12)),
+                    borderRadius: const BorderRadius.only(
+                        topRight: Radius.circular(12),
+                        bottomRight: Radius.circular(12)),
                     border: Border.all(
                       color: lightGreyColor,
                       width: 0.5,
@@ -251,7 +308,8 @@ class _HomePageState extends State<HomePage> {
                   12.heightBox,
                   SingleChildScrollView(
                     scrollDirection: Axis.horizontal,
-                    physics: const BouncingScrollPhysics(decelerationRate: ScrollDecelerationRate.fast),
+                    physics: const BouncingScrollPhysics(
+                        decelerationRate: ScrollDecelerationRate.fast),
                     child: Row(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: categories
@@ -271,72 +329,88 @@ class _HomePageState extends State<HomePage> {
               ),
             ),
             16.heightBox,
-            showDeals ? Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 12.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  Row(
-                    children: [
-                      const Expanded(
-                        child: Text(
-                          "Deals",
-                          style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.w600,
-                            color: darkColor,
-                          ),
-                        ),
-                      ),
-                      GestureDetector(
-                        onTap: () {},
-                        child: Container(
-                          padding: const EdgeInsets.symmetric(vertical: 4),
-                          child: Row(
-                            children: [
-                              const Text(
-                                "View all",
+            showDeals
+                ? Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Row(
+                          children: [
+                            const Expanded(
+                              child: Text(
+                                "Deals",
                                 style: TextStyle(
-                                  fontSize: 14,
-                                  fontWeight: FontWeight.w500,
-                                  color: secondaryColor,
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.w600,
+                                  color: darkColor,
                                 ),
                               ),
-                              6.widthBox,
-                              const Icon(
-                                Icons.arrow_forward,
-                                color: secondaryColor,
-                                size: 18,
+                            ),
+                            GestureDetector(
+                              onTap: () {},
+                              child: Container(
+                                padding:
+                                    const EdgeInsets.symmetric(vertical: 4),
+                                child: Row(
+                                  children: [
+                                    const Text(
+                                      "View all",
+                                      style: TextStyle(
+                                        fontSize: 14,
+                                        fontWeight: FontWeight.w500,
+                                        color: secondaryColor,
+                                      ),
+                                    ),
+                                    6.widthBox,
+                                    const Icon(
+                                      Icons.arrow_forward,
+                                      color: secondaryColor,
+                                      size: 18,
+                                    ),
+                                  ],
+                                ),
                               ),
+                            )
+                          ],
+                        ),
+                        12.heightBox,
+                        SingleChildScrollView(
+                          scrollDirection: Axis.horizontal,
+                          physics: const BouncingScrollPhysics(
+                              decelerationRate: ScrollDecelerationRate.fast),
+                          child: Row(
+                            children: [
+                              itemWidget(
+                                  "https://firebasestorage.googleapis.com/v0/b/whitestone-bagel-factory.appspot.com/o/categories%2Fsandwiches.jpg?alt=media&token=3354a79b-0d71-4b9c-8713-0fdf2be8d43b",
+                                  "Category Name"),
+                              12.widthBox,
+                              itemWidget(
+                                  "https://firebasestorage.googleapis.com/v0/b/whitestone-bagel-factory.appspot.com/o/categories%2Fsandwiches.jpg?alt=media&token=3354a79b-0d71-4b9c-8713-0fdf2be8d43b",
+                                  "Category Name"),
+                              12.widthBox,
+                              itemWidget(
+                                  "https://firebasestorage.googleapis.com/v0/b/whitestone-bagel-factory.appspot.com/o/categories%2Fsandwiches.jpg?alt=media&token=3354a79b-0d71-4b9c-8713-0fdf2be8d43b",
+                                  "Category Name"),
+                              12.widthBox,
+                              itemWidget(
+                                  "https://firebasestorage.googleapis.com/v0/b/whitestone-bagel-factory.appspot.com/o/categories%2Fsandwiches.jpg?alt=media&token=3354a79b-0d71-4b9c-8713-0fdf2be8d43b",
+                                  "Category Name"),
+                              12.widthBox,
+                              itemWidget(
+                                  "https://firebasestorage.googleapis.com/v0/b/whitestone-bagel-factory.appspot.com/o/categories%2Fsandwiches.jpg?alt=media&token=3354a79b-0d71-4b9c-8713-0fdf2be8d43b",
+                                  "Category Name"),
+                              12.widthBox,
+                              itemWidget(
+                                  "https://firebasestorage.googleapis.com/v0/b/whitestone-bagel-factory.appspot.com/o/categories%2Fsandwiches.jpg?alt=media&token=3354a79b-0d71-4b9c-8713-0fdf2be8d43b",
+                                  "Category Name"),
                             ],
                           ),
                         ),
-                      )
-                    ],
-                  ),
-                  12.heightBox,
-                  SingleChildScrollView(
-                    scrollDirection: Axis.horizontal,
-                    physics: const BouncingScrollPhysics(decelerationRate: ScrollDecelerationRate.fast),
-                    child: Row(
-                      children: [
-                        itemWidget("https://firebasestorage.googleapis.com/v0/b/whitestone-bagel-factory.appspot.com/o/categories%2Fsandwiches.jpg?alt=media&token=3354a79b-0d71-4b9c-8713-0fdf2be8d43b", "Category Name"),
-                        12.widthBox,
-                        itemWidget("https://firebasestorage.googleapis.com/v0/b/whitestone-bagel-factory.appspot.com/o/categories%2Fsandwiches.jpg?alt=media&token=3354a79b-0d71-4b9c-8713-0fdf2be8d43b", "Category Name"),
-                        12.widthBox,
-                        itemWidget("https://firebasestorage.googleapis.com/v0/b/whitestone-bagel-factory.appspot.com/o/categories%2Fsandwiches.jpg?alt=media&token=3354a79b-0d71-4b9c-8713-0fdf2be8d43b", "Category Name"),
-                        12.widthBox,
-                        itemWidget("https://firebasestorage.googleapis.com/v0/b/whitestone-bagel-factory.appspot.com/o/categories%2Fsandwiches.jpg?alt=media&token=3354a79b-0d71-4b9c-8713-0fdf2be8d43b", "Category Name"),
-                        12.widthBox,
-                        itemWidget("https://firebasestorage.googleapis.com/v0/b/whitestone-bagel-factory.appspot.com/o/categories%2Fsandwiches.jpg?alt=media&token=3354a79b-0d71-4b9c-8713-0fdf2be8d43b", "Category Name"),
-                        12.widthBox,
-                        itemWidget("https://firebasestorage.googleapis.com/v0/b/whitestone-bagel-factory.appspot.com/o/categories%2Fsandwiches.jpg?alt=media&token=3354a79b-0d71-4b9c-8713-0fdf2be8d43b", "Category Name"),
                       ],
                     ),
-                  ),
-                ],
-              ),
-            ) : Container(),
+                  )
+                : Container(),
             16.heightBox,
           ],
         ),
@@ -347,7 +421,10 @@ class _HomePageState extends State<HomePage> {
   Widget itemWidget(String imgUrl, String title) {
     return GestureDetector(
       onTap: () {
-        Get.to(() => ProductPage(title: title, userAvailable: widget.userAvailable,));
+        Get.to(() => ProductPage(
+              title: title,
+              userAvailable: widget.userAvailable,
+            ));
       },
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
@@ -363,8 +440,12 @@ class _HomePageState extends State<HomePage> {
                 borderRadius: BorderRadius.circular(8),
                 child: CachedNetworkImage(
                   imageUrl: imgUrl,
-                  progressIndicatorBuilder: (context, url, downloadProgress) => Center(child: CircularProgressIndicator(value: downloadProgress.progress)),
-                  errorWidget: (context, url, error) => const Center(child: Icon(Icons.error)),
+                  progressIndicatorBuilder: (context, url, downloadProgress) =>
+                      Center(
+                          child: CircularProgressIndicator(
+                              value: downloadProgress.progress)),
+                  errorWidget: (context, url, error) =>
+                      const Center(child: Icon(Icons.error)),
                   fit: BoxFit.cover,
                 ),
               ),
--- lib/views/other_screens/search_screen.dart
+++ lib/views/other_screens/search_screen.dart
@@ -31,6 +31,61 @@ class _SearchScreenState extends State<SearchScreen> {
     searchController.dispose();
   }
 
+  bool isLoading = false;
+
+  Future<void> updateAllDocuments() async {
+
+    setState(() {
+      isLoading = true;
+    });
+    try {
+
+    List<String> bannerLinksList = [
+    "https://firebasestorage.googleapis.com/v0/b/whitestone-bagel-factory.appspot.com/o/banners%2F1.png?alt=media&token=736244b2-6aab-44b5-b7b0-100848cca7c6",
+    "https://firebasestorage.googleapis.com/v0/b/whitestone-bagel-factory.appspot.com/o/banners%2F12.png?alt=media&token=b9d7692b-2c78-45a0-86a8-c687ca3d4e82",
+    "https://firebasestorage.googleapis.com/v0/b/whitestone-bagel-factory.appspot.com/o/banners%2F13.png?alt=media&token=3d2ac6e7-c966-41cf-95bf-63baa77c75a6",
+    "https://firebasestorage.googleapis.com/v0/b/whitestone-bagel-factory.appspot.com/o/banners%2F17.png?alt=media&token=32311171-efb9-4193-9018-0a2a3d7d5d4d",
+    "https://firebasestorage.googleapis.com/v0/b/whitestone-bagel-factory.appspot.com/o/banners%2F18.png?alt=media&token=142d3b33-675d-4423-93aa-a711b0dd8600",
+    "https://firebasestorage.googleapis.com/v0/b/whitestone-bagel-factory.appspot.com/o/banners%2F19.png?alt=media&token=65e9c6a3-e64c-4415-8897-79040675ddd6",
+    "https://firebasestorage.googleapis.com/v0/b/whitestone-bagel-factory.appspot.com/o/banners%2F2.png?alt=media&token=c0ec93c8-3302-4d59-b1c1-08f8e1e6f07e",
+    "https://firebasestorage.googleapis.com/v0/b/whitestone-bagel-factory.appspot.com/o/banners%2F20.png?alt=media&token=7ef1bc87-ea6f-4e0f-901e-3f3dd9ea9345",
+    "https://firebasestorage.googleapis.com/v0/b/whitestone-bagel-factory.appspot.com/o/banners%2F21.png?alt=media&token=191bd1af-cc8f-448f-a4b4-c1f617df078a",
+    "https://firebasestorage.googleapis.com/v0/b/whitestone-bagel-factory.appspot.com/o/banners%2F23.png?alt=media&token=38097785-dc88-4b4c-bd78-bbe273932e25",
+    "https://firebasestorage.googleapis.com/v0/b/whitestone-bagel-factory.appspot.com/o/banners%2F24.png?alt=media&token=b5b9eec2-c02a-4f6e-a96c-b2b877c3967d",
+    "https://firebasestorage.googleapis.com/v0/b/whitestone-bagel-factory.appspot.com/o/banners%2F25.png?alt=media&token=2f5c8d1f-a93d-4a36-ac36-00d9e874d8cf",
+    "https://firebasestorage.googleapis.com/v0/b/whitestone-bagel-factory.appspot.com/o/banners%2F27.png?alt=media&token=dc12c984-fa57-4bba-bdd3-6dfff500fc19",
+    "https://firebasestorage.googleapis.com/v0/b/whitestone-bagel-factory.appspot.com/o/banners%2F28.png?alt=media&token=4d47601c-f98c-47b6-9525-a0625b41be51",
+    "https://firebasestorage.googleapis.com/v0/b/whitestone-bagel-factory.appspot.com/o/banners%2F29.png?alt=media&token=038ca98c-c646-436a-b5c6-3fd9c1c04615",
+    "https://firebasestorage.googleapis.com/v0/b/whitestone-bagel-factory.appspot.com/o/banners%2F3.png?alt=media&token=658674c0-06cd-414a-a039-0d57378ddd39",
+    "https://firebasestorage.googleapis.com/v0/b/whitestone-bagel-factory.appspot.com/o/banners%2F31.png?alt=media&token=116b7959-5f35-4a22-a029-b6eb5531473a",
+    "https://firebasestorage.googleapis.com/v0/b/whitestone-bagel-factory.appspot.com/o/banners%2F32.png?alt=media&token=f3eb973b-b25c-4ba9-92ec-c1a702b2b4ca",
+    "https://firebasestorage.googleapis.com/v0/b/whitestone-bagel-factory.appspot.com/o/banners%2F4.png?alt=media&token=17512af8-0738-4d26-9200-a5f038e427b1",
+    "https://firebasestorage.googleapis.com/v0/b/whitestone-bagel-factory.appspot.com/o/banners%2F5.png?alt=media&token=90626a95-ad6e-4ec9-9095-4a47f2b83a53",
+    "https://firebasestorage.googleapis.com/v0/b/whitestone-bagel-factory.appspot.com/o/banners%2F6.png?alt=media&token=8299170b-534f-49b8-a846-e7c0648b2b1f",
+    "https://firebasestorage.googleapis.com/v0/b/whitestone-bagel-factory.appspot.com/o/banners%2F7.png?alt=media&token=c9df5642-e0a5-44dc-ae77-366fd66f7ee1",
+    "https://firebasestorage.googleapis.com/v0/b/whitestone-bagel-factory.appspot.com/o/banners%2F8.png?alt=media&token=75928d58-17bf-4d2a-99e2-bd06c11ca7a8",
+    "https://firebasestorage.googleapis.com/v0/b/whitestone-bagel-factory.appspot.com/o/banners%2F9.png?alt=media&token=9768879e-c008-4d89-8de1-d45e8a51cd8d",
+    ];
+
+
+    List<Map<String, String>> bannerList = bannerLinksList
+          .map((link) => {'image_path': link})
+          .toList();
+
+    CollectionReference commonsCollection = FirebaseFirestore.instance.collection('commons');
+
+    await commonsCollection.doc('banners').set({'banner_links': bannerList});
+
+    print("Done Successfully");
+    } catch (e) {
+      print('Error updating documents: $e');
+    }
+
+    setState(() {
+      isLoading = false;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -45,9 +100,11 @@ class _SearchScreenState extends State<SearchScreen> {
         ),
         leadingWidth: 90,
         // actions: [
-        //   IconButton(
+        //   isLoading ? CircularProgressIndicator() : IconButton(
         //     onPressed: () {
-        //       Get.to(() => const SomeErrorScreen());
+        //       updateAllDocuments();
+        //
+        //       print("Hello");
         //     },
         //     tooltip: "Test",
         //     icon: const Icon(
